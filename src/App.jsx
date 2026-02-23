59b72ad445ea7d074863b74ecb607e13736d13c3
Juan Cruz <juancbonelli@gmail.com>
2026-02-23 10:39:34 +0100

    ejercicio resuelto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
- //Aquí tu código
+  const [name, setName] = useState("Sofía");
+  const [newName, setNewName] = useState("");
+
+  const changeName = (e) => {
+    e.preventDefault();
+    if (newName.trim() === "") return;
+    setName(newName);
+    setNewName("");
+  };
+
+  return (
+    <div className="App">
+      <h2>Teacher Name {name}</h2>
+      <ul>
+        <li onClick={() => setName("Data")}>Data</li>
+        <li onClick={() => setName("Reyes")}>Reyes</li>
+        <li onClick={() => setName("Yolanda")}>Yolanda</li>
+      </ul>
+      <form onSubmit={changeName}>
+        <input
+          type="text"
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+        />
+        <button type="submit">Change Name</button>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
